2eb0aa9c77aad53142b9c7b7717dcc5e221c4a5c
Ahmet Bakırcan <ahmetbakircan@gmail.com>
2024-11-14 14:05:11 +0300

    ex04 Done

--- ex04/srcs/main.cpp
+++ ex04/srcs/main.cpp
@@ -6,15 +6,17 @@
 /*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 18:02:54 by abakirca          #+#    #+#             */
-/*   Updated: 2024/11/11 18:03:54 by abakirca         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:09:48 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "replacer.hpp"
 
 int main(int ac, char **av)
 {
 	if (ac != 4)
 		std::cout << "Invalid input!" << std::endl;
+	else
+		replace(av);
 	
 }
---
+++ ex04/srcs/replacer.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replacer.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 13:06:02 by abakirca          #+#    #+#             */
+/*   Updated: 2024/11/14 13:17:00 by abakirca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REPLACER_HPP
+#define REPLACER_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void	replace(char **av);
+
+#endif
