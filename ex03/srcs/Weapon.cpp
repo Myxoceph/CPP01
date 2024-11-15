52263343c6f37ef47eaa5ef55c809c63f16327a7
Ahmet Bakırcan <ahmetbakircan@gmail.com>
2024-11-15 16:25:33 +0300

    Last changes

--- ex03/srcs/Weapon.cpp
+++ ex03/srcs/Weapon.cpp
@@ -6,13 +6,13 @@
 /*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:17:47 by abakirca          #+#    #+#             */
-/*   Updated: 2024/11/11 17:31:58 by abakirca         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:15:21 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
-const std::string Weapon::getType()
+const std::string &Weapon::getType()
 {
 	return (this->type);
 }
--- ex03/srcs/Weapon.hpp
+++ ex03/srcs/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: abakirca <abakirca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:05:09 by abakirca          #+#    #+#             */
-/*   Updated: 2024/11/11 17:41:19 by abakirca         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:15:17 by abakirca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class Weapon
 	private:
 		std::string type;
 	public:
-		const std::string getType();
+		const std::string &getType();
 		void setType(std::string wptype);
 		Weapon(std::string name);
 };
